42211e9b1cbbd4f2ff373485590800d109e8edd6
IslamElbeltagye <eslamelbeltagy1111@gmail.com>
2023-07-15 21:17:50 +0300

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ * This is The entry Function
+ *
+ * Desription: A C program That print with function
+ *
+ * return: Always 0 (Success)
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-
 	return (0);
 }
