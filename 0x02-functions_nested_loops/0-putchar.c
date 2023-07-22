0c4b0f539a6d7412f8e0f5578499bb5479370833
IslamElbeltagye <eslamelbeltagy1111@gmail.com>
2023-07-22 05:13:14 +0300

    print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-
 /**
  * main - Entry Point
+ *
+ * @c: The character to print
  *
  * Description: function to print putchar
  *
  * Return: Always 0 (Success)
 */
+
+int _putchar(char c);
 
 int main(void)
 {
