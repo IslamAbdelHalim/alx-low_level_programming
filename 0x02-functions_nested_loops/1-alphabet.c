19a36baaf61433b2b4d9e7beeb0ea717d243833d
IslamElbeltagye <eslamelbeltagy1111@gmail.com>
2023-07-23 14:09:34 +0300

    betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- *
  * print_alphabet - utllizes the putchar funtion to print
- * 		The alphabet a -z
+ *	the alphabet a -z
 */
 
 void print_alphabet(void)
 {
 	int ch;
-	
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 		_putchar(ch);
 	_putchar('\n');
