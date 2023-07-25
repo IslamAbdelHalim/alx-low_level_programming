a7ed4beb7ba263f8c594b609d1fe2376cddb90a9
IslamElbeltagye <eslamelbeltagy1111@gmail.com>
2023-07-25 18:29:30 +0300

    add -

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10: any thing intial shor description
+ * print_alphabet_x10 - This is a function to print
+ *	This file contain function to print alphabet from a - z 10 times
  *
- * print_alphabet_x10: to print all alphabet from a - z
- *
- *  prototype: print alphabet 10 time
+ * prototype: print alphabet 10 time
 */
 
 void print_alphabet_x10(void)
