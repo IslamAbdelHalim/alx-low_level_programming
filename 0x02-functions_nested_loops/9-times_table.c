457c7db9e14205ef69dcf91948bcd9c627b6c8e2
IslamElbeltagye <eslamelbeltagy1111@gmail.com>
2023-07-26 01:05:12 +0300

    any

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - function to print time tables
+*/
+
+void times_table(void)
+{
+	int i, j, num;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(48);
+		for (j = 1; j < 10; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			
+			num = i * j;
+
+			if (num <= 9)
+				_putchar(' ');
+			else
+				_putchar((num / 10) + 48);
+			
+			_putchar((num % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
