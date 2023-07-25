2accd12a956a8a981ef35e0aae3968d0b792fd12
IslamElbeltagye <eslamelbeltagy1111@gmail.com>
2023-07-26 01:16:22 +0300

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - function to add two intexgers
+ *
+ * @a: first intergers
+ *
+ * @b: second integers
+ * 
+ * Return : return the value
+*/
+
+int add (int a, int b)
+{
+	return (a + b);
+}
