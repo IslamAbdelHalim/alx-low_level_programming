e12fc2fc84d6a53719e458765c2cb44273f4c422
IslamElbeltagye <eslamelbeltagy1111@gmail.com>
2023-08-02 17:59:00 +0300

    a

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (sucess)
+*/
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++) /*This for the first number */
+	{
+		for (j = 1; j <= 9; j++) /*This for the second number*/
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
