7b41df7e3956b6a9d06da8841186827683098399
IslamElbeltagye <eslamelbeltagy1111@gmail.com>
2023-08-04 18:16:04 +0300

    a

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - fucntion to print a string reverse
+ *
+ * @s: The array of character
+*/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
