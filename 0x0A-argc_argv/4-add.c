dc11862d709a0589e0f3a3f14d402846f46f6f53
IslamElbeltagye <eslamelbeltagy1111@gmail.com>
2023-08-08 06:09:25 +0300

    a

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point to print the name of program
+ *
+ * @argc: The counter of argument
+ *
+ * @argv: argument vector
+ *
+ * Return: 0 Always success
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		int i, sum;
+
+		sum = 0;
+
+		for (i = 1; i < argc; i++)
+		{
+			int num;
+
+			num = atoi(argv[i]);
+			if (num == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				sum += num;
+			}
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
