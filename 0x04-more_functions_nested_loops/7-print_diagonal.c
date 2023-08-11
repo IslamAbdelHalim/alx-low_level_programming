70f073a7fc4b8c5b72697899bcfc04a33ef5bea4
IslamElbeltagye <eslamelbeltagy1111@gmail.com>
2023-08-11 18:42:27 +0300

    a

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,10 +17,10 @@ void print_diagonal(int n)
 	}
 	else
 	{
-		for (i = 1; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
-			for (j = 1; j <= i; j++)
-				_putchar(' ');
+			for (j = 0; j < i; j++)
+				_putchar(32);
 			_putchar(92);
 			_putchar('\n');
 		}
