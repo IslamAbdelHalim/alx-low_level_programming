5bfb65215cced11f818f04faae5447dcaab60901
IslamElbeltagye <eslamelbeltagy1111@gmail.com>
2023-08-11 19:12:26 +0300

    a

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,17 +10,21 @@ void print_triangle(int size)
 {
 	int space, hash, line;
 
-	for (line = 0; line < size; line++)
-	{
-		for (space = line; space < size; space++)
-		{
-			_putchar(32);
-		}
-
-		for (hash = 0; hash <= line; hash++)
-		{
-			_putchar(35);
-		}
+	if (size <= 0)
 		_putchar('\n');
-	}
+	else
+
+		for (line = 0; line < size; line++)
+		{
+			for (space = line; space < size; space++)
+			{
+				_putchar(32);
+			}	
+
+			for (hash = 0; hash <= line; hash++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
 }
