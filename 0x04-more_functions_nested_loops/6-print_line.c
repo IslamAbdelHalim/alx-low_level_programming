b3a90b85731f894311b00021878e679d059447b5
IslamElbeltagye <eslamelbeltagy1111@gmail.com>
2023-08-11 15:34:07 +0300

    a

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * Print_line - Function That print line using _
+ *
+ * @n: The length of line or the number of _
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
