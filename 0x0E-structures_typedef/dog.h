bc7e499c1e3f4ef134f648e7561ce6e2d5d0e1d3
IslamElbeltagye <eslamelbeltagy1111@gmail.com>
2023-08-15 07:37:36 +0300

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struction to info dog
+ *
+ * @name: his name
+ *
+ * @owner: his owner
+ *
+ * @age: his age
+*/
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
