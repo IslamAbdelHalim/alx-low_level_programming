3add2dfecec4eea87f10169d7042ed58021cf047
IslamElbeltagye <eslamelbeltagy1111@gmail.com>
2023-08-15 08:13:59 +0300

    a

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -19,7 +19,10 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	struct dog;
 
-	d->name = name;
-	d->owner = owner;
-	d->age = age;
+	if (d)
+	{
+		d->name = name;
+		d->owner = owner;
+		d->age = age;
+	};
 }
