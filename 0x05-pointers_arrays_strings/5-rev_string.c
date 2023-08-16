2422de3ea499155cec6f5fe6281e376e56cac866
IslamElbeltagye <eslamelbeltagy1111@gmail.com>
2023-08-16 19:50:35 +0300

    a

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,15 +12,12 @@ void rev_string(char *s)
 	int i, n;
 	char temp;
 		i = 0;
-	
-	while(s[i])
+	while (s[i])
 		i++;
-
 	for (n = 0; n < i / 2; n++)
 	{
 		temp = s[n];
 		s[n] = s[i - 1 - n];
-		s[i - 1- n] = temp;
+		s[i - 1 - n] = temp;
 	}
-	
 }
