3b7522ca2aeac8eb9f13f73ec07f76072a789192
IslamElbeltagye <eslamelbeltagy1111@gmail.com>
2023-08-16 16:57:24 +0300

    a

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - function to print half of string
+ *
+ * @str: the character
+*/
+
+void puts_half(char *str)
+{
+	int i;
+
+		i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+
+	for (i /= 2; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
