488906d7a104ab866257d169c0342aad4b11f22a
IslamElbeltagye <eslamelbeltagy1111@gmail.com>
2023-08-16 18:32:59 +0300

    a

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * _strcpy - function that copies string from var1 to var2
@@ -14,18 +15,15 @@
 char *_strcpy(char *dest, char *src)
 {
 	int s, d;
-	char *ptr;
 
 	if (src == NULL)
 		printf("\n");
 
 	for (s = 0; src[s] != '\0'; s++)
 		;
-	s++;
 	for (d = 0; d <= s; d++)
 	{
 		dest[d] = src[d];
 	}
-	ptr = dest;
-	return (ptr);
+	return (dest);
 }
