c5a565c0921160305c8d62dedeb7133161281fec
IslamElbeltagye <eslamelbeltagy1111@gmail.com>
2023-08-16 14:39:01 +0300

    a

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -5,12 +5,15 @@
 /**
  * dog_t *new_dog - is a alias strucutre for struct dog
  *
+ * new_dog - The function to create a new dog
+ *
  * @name: the name of dog
  *
  * @age: his age
  *
  * @owner: his owner
  *
+ * Return: newDog info and if null return null
 */
 
 dog_t *new_dog(char *name, float age, char *owner)
@@ -22,6 +25,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 		newDog->name = name;
 		newDog->age = age;
 		newDog->owner = owner;
+		return (newDog);
 	}
-	return newDog;
+	else
+	{
+		return (NULL);
+	}
 }
