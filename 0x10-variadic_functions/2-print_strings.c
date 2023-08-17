ee0834950a0897d24de283e1d5866051faa96906
IslamElbeltagye <eslamelbeltagy1111@gmail.com>
2023-08-18 00:06:04 +0300

    a

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,16 +13,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	char *s;
-
 	va_list name;
-
-	va_start(name, n);
 
 	if (n == 0)
 	{
 		printf("\n");
 		return;
 	}
+	va_start(name, n);
 	for (i = 0; i < n; i++)
 	{
 		s = va_arg(name, char *);
