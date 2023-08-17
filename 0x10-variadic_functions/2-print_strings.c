04b8a3031fd0c20a1f5b8977f5ebcab641e4b499
IslamElbeltagye <eslamelbeltagy1111@gmail.com>
2023-08-18 00:15:08 +0300

    a

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,14 +24,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		s = va_arg(name, char *);
-		if (s == NULL)
-		{
-			printf("(nil)");
-		}
-		else
-		{
-			printf("%s%s", s, (i < n - 1) ? (separator ? separator : "") : "\n");
-		}
+		printf("%s%s", (s ? s : "(nil)"), (i < n - 1) ?
+				(separator ? separator : "") : "\n");
 	}
 	va_end(name);
 }
