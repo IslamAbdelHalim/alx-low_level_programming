fea88e3ed2f8b1802ba383a78c1464e4f3e1e895
IslamElbeltagye <eslamelbeltagy1111@gmail.com>
2023-08-17 22:06:49 +0300

    a

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_strings - function that prints any number of string
+ *
+ * @separator: separator argument
+ *
+ * @n: number of argument
+*/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *s;
+
+	va_list name;
+
+	va_start(name, n);
+
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(name, char *);
+		if (s == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s%s", s, (i < n - 1) ? separator : "");
+		}
+	}
+	va_end(name);
+	printf("\n");
+}
