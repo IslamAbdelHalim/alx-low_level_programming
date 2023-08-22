20e36e4f106c0e1d7387feb536100f29ea6930d5
islam_ahmed <eslamelbeltagye@gmail.com>
2023-08-22 04:40:31 +0300

    a

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,10 +11,13 @@ void print_times_table(int n)
 {
 	int i, j, num;
 
-	for (i = 0; i < n; i++)
+	if (n > 15 || n < 0)
+		return;
+
+	for (i = 0; i <= n; i++)
 	{
 		_putchar('0');
-		for (j = 1; j < n; j++)
+		for (j = 1; j <= n; j++)
 		{
 			_putchar(',');
 			_putchar(' ');
