c75d8145f6eb26df8dee2b04eb4c68a22777a34a
islam_ahmed <eslamelbeltagye@gmail.com>
2023-08-23 00:12:58 +0300

    a

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	int i, pf;
+
+	long int num = 612852475143;
+
+	pf = 0;
+
+	i = 3;
+
+	while (num % 2 == 0)
+		num /= 2;
+
+	while (num > 1)
+	{
+		num /= i;
+		pf = i;
+		i += 2;
+	}
+	printf("%d\n", pf);
+	return (0);
+}
