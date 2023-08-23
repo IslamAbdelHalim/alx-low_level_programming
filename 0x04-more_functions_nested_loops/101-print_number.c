bd9899b783e2f61a871945bf9025f8850d45c313
islam_ahmed <eslamelbeltagye@gmail.com>
2023-08-24 00:24:56 +0300

    a

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - function to print number
+ *
+ * @n: The number
+*/
+
+void print_number(int n)
+{
+	/* check if n less than 0 */
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	/* reduce the number untile become on digit */
+	if (n / 10)
+	{
+		/*if(n / 10) > 10 make the same functio */
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
+}
