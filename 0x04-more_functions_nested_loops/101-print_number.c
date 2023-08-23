5d9470c7405e83ac83029d5433167be3416f38eb
islam_ahmed <eslamelbeltagye@gmail.com>
2023-08-24 00:33:04 +0300

    a

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -16,10 +16,10 @@ void print_number(int n)
 	}
 
 	/* reduce the number untile become on digit */
-	if (n / 10)
+	if ((n / 10) > 0)
 	{
 		/*if(n / 10) > 10 make the same functio */
 		print_number(n / 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar((n % 10) + '0');
 }
