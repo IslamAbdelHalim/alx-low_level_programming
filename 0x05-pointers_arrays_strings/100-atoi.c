99531329bc2857009bc90eb97e1e59fca44ebc97
islam_ahmed <eslamelbeltagye@gmail.com>
2023-09-07 01:01:18 +0300

    a

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,12 @@
 #include "main.h"
 
-/***/
+/**
+ * _atoi - fucntion that converst the string into int
+ *
+ * @s: The string
+ *
+ * Return: The integer
+*/
 
 int _atoi(char *s)
 {
@@ -10,10 +16,9 @@ int _atoi(char *s)
 		if (*s == '-')
 			sign *= -1;
 		else if (*s >= '0' && *s <= '9')
-			num = (num *10) + (*s - '0');
+			num = (num * 10) + (*s - '0');
 		else if (num > 0)
 			break;
-	}while (*s++);
-		
+	} while (*s++);
 	return (num * sign);
 }
