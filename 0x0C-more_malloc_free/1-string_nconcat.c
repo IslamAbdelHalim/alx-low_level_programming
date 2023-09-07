3637e91dc05389971661a5628d9b34e96369805c
islam_ahmed <eslamelbeltagye@gmail.com>
2023-09-08 00:09:32 +0300

    a

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -43,7 +43,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (n <= 0)
+	if ((int)n < 0)
+		return (NULL);
+	if ((int)n >= _strlen(s2))
 		n = _strlen(s2);
 
 	/*Allocat The first string memory*/
