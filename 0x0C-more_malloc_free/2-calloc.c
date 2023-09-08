88e6c430598ff0782a2d5b2cd42b8d325916a97d
islam_ahmed <eslamelbeltagye@gmail.com>
2023-09-08 12:31:17 +0300

    a

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -20,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	arr = malloc(nmemb * sizeof(int));
+	arr = malloc(nmemb * size);
 	if (arr == NULL)
 		return (NULL);
 
