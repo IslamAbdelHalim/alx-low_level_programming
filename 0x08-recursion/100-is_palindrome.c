d17ff9abd92f03adde2657889af59851e010f6d5
islam_ahmed <eslamelbeltagye@gmail.com>
2023-09-10 20:14:52 +0300

    a

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+/**
+ * length - function to know the lenght of string
+ *
+ * @s: array of character (string)
+ *
+ * Return: The lenght of string
+*/
+
+int length(char *s)
+{
+	int size = 0;
+
+	while (*s != '\0')
+	{
+		size++;
+		s++;
+	}
+	return (size);
+}
+
+/**
+ * is_palindromerec - function to check two character
+ *
+ * @s: The new string
+ *
+ * @start: The first char
+ *
+ * @end: The second char
+ *
+ * Return: If the two char equal or not
+*/
+int is_palindromerec(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+	if (s[start] != s[end])
+		return (0);
+	return (is_palindromerec(s, start + 1, end - 1));
+}
+
+/**
+ * is_palindrome - function to check is the string
+ * is palindroma or not
+ *
+ * @s: The string
+ *
+ * Return: 1 if the string is palindrome and 0 if is not
+*/
+
+int is_palindrome(char *s)
+{
+	int size = length(s);
+
+	if (size == 0 || size == 1)
+		return (1);
+
+	return (is_palindromerec(s, 0, size - 1));
+}
