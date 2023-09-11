f488addfeb145f4ad7596914ee9f0330c0600777
islam_ahmed <eslamelbeltagye@gmail.com>
2023-09-12 00:37:39 +0300

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure to store info dog
+ *
+ * @name: The name of dog
+ *
+ * @age: His age
+ *
+ * @owner: His owner
+*/
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}dog_t;
+
+#endif
