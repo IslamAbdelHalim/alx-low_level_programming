d9ad09fa1f130a4845d21d92143614a6bf7fabea
islam_ahmed <eslamelbeltagye@gmail.com>
2023-09-14 23:52:48 +0300

    a

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,76 @@
+#include <calc.h>
+
+/**
+ * op_add - function to make add operation
+ *
+ * @a: The first number
+ *
+ * @b: The second number
+ *
+ * Return: The result
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function to make add operation
+ *
+ * @a: The first number
+ *
+ * @b: The second number
+ *
+ * Return: The result
+*/
+
+int op_sub(int a, int b)
+{
+        return (a - b);
+}	
+
+/**
+ * op_mul - function to make add operation
+ *
+ * @a: The first number
+ *
+ * @b: The second number
+ *
+ * Return: The result
+*/
+
+int op_mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * op_div - function to make add operation
+ *
+ * @a: The first number
+ *
+ * @b: The second number
+ *
+ * Return: The result
+*/
+
+int op_div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * op_mod - function to make add operation
+ *
+ * @a: The first number
+ *
+ * @b: The second number
+ *
+ * Return: The result
+*/
+
+int op_mod(int a, int b)
+{
+        return (a % b);
+}
