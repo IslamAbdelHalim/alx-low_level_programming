d5cd434b2cdd52e6d536e4bc677336c31764a437
IslamAhmed <eslamelbeltagye1111@gmail.com>
2023-09-28 22:26:02 +0300

    a

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,41 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - function that know length of array
- *
- * @str: array of string
- *
- * Return: The array of string
-*/
-
-int _strlen(const char *str)
-{
-	int i = 0;
-
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-/**
- * powBit - function That make power to two
- *
- * @n: The number of power
- *
- * Return: The power of two
-*/
-
-int powBit(int n)
-{
-	int num = 1;
-	int i;
-
-	for (i = 0; i < n; i++)
-		num = 2 * num;
-	return (num);
-}
-
-/**
  * binary_to_uint - function convert binary to int
  *
  * @b: The binary number
@@ -46,25 +11,14 @@ int powBit(int n)
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
-	int length = _strlen(b) - 1;
-	int i;
-	int pow = 0;
 
 	if (!b)
 		return (0);
-
-	for (i = length; i >= 0; i--)
+	while (*b)
 	{
-		if (b[i] != '0' || b[i] != '1')
-		{
-			if (b[i] == '1')
-				num = num + powBit(pow);
-			pow++;
-		}
-		else
-		{
+		if (*b != '0' && *b != '1')
 			return (0);
-		}
+		num = num * 2 + (*b++ - '0');
 	}
 	return (num);
 }
