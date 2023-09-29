b2e0d339c27f063e67555ed5292469750056b786
IslamAhmed <eslamelbeltagye1111@gmail.com>
2023-09-29 10:39:57 +0300

    a

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * get_bit - function to git the bit
+ *
+ * @n: The number
+ *
+ * @index: The index of wanted bit
+ *
+ * Return: The bit
+*/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (!n)
+		return (-1);
+	return (n >> index & 1);
+}
