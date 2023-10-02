609c3ce3e00d71b6d26ff9245e66c82fa05fd33a
IslamAhmed <eslamelbeltagye1111@gmail.com>
2023-10-02 23:10:10 +0300

    a

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * _strlen - function that calculat length
+ *
+ * @str: array of string
+ *
+ * Return: The lenght
+*/
+
+int _strlen(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+
+	while (*str++)
+		i++;
+	return (i);
+}
+
+/**
+ * append_text_to_file - function that append text
+ *
+ * @filename: The file which we append text
+ *
+ * @text_content: The text which add
+ *
+ * Return: 1 if success -1 if fail
+*/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes = 0, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (len)
+		bytes = write(fd, text_content, len);
+	close(fd);
+	return (bytes == len ? 1 : -1);
+}
