e446e9d4a2aa1701b1433eb6977c32ad6f84e38e
IslamAhmed <eslamelbeltagye1111@gmail.com>
2023-10-22 21:34:56 +0300

    a

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
 /**
+ * num_length - return the length of string
+ *
+ * @num: operand number
+ *
+ * Return: number of digits
+*/
+
+int num_length(int num)
+{
+	int length = 0;
+
+	if (!num)
+		return (0);
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+	return (length);
+}
+
+/**
  * main - Entry point
  *
  * Return: Always 0
@@ -8,23 +30,34 @@
 
 int main(void)
 {
-	int i;
-	unsigned long fsum, nsum, lsum;
-
-	fsum = 0;
-	nsum = 1;
-	lsum = 0;
+	int i, c;
+	unsigned long num1 = 1, num2 = 2, lsum,
+		      max = 1000000, flo = 0, f2o = 0, sum = 0;
 
 	for (i = 1; i <= 98; i++)
 	{
-		lsum = fsum + nsum;
-		fsum = nsum;
-		nsum = lsum;
-		if (i == 95)
-			printf("%ld", lsum);
+		if (flo > 0)
+			printf("%lu", flo);
+		c = num_length(max) - 1 - num_length(num1);
+
+		while (flo > 0 && c > 0)
+		{
+			printf("%d", 0);
+			c--;
+		}
+		printf("%lu", num1);
+
+		lsum = (num1 + num2) % max;
+		sum = flo + f2o + (num1 + num2) / max;
+		num1 = num2;
+		flo = f2o;
+		num2 = lsum;
+		f2o = sum;
+
+		if (i != 98)
+			printf(", ");
 		else
-			printf("%ld, ", lsum);
+			printf("\n");
 	}
-	printf("\n");
 	return (0);
 }
