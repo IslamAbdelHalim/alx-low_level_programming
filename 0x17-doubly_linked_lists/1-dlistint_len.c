061809a05a6a1b66c950c11b0a3023b3a35e99c2
IslamAhmed <eslamelbeltagye1111@gmail.com>
2023-11-10 01:18:10 +0200

    a

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - function that return the number of elements
+ *
+ * @h: The list
+ *
+ * Return: The number of elements
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while(h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
