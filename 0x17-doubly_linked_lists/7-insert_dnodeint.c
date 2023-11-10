7cb599640e0f20078b13b87a6b1600a4453faa24
IslamAhmed <eslamelbeltagye1111@gmail.com>
2023-11-10 23:39:52 +0200

    a

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -48,7 +48,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	 * If the index is the last index of list
 	 * then add the new node at the end point
 	 */
-	if (++i == idx)
+	if (temp == NULL)
 		return (add_dnodeint_end(h, n));
 	/*else if the index is more than list or no index then return null*/
 	return (NULL);
