6aae31c2812ea540d218197a7ede01c9f751c89f
IslamAhmed <eslamelbeltagye1111@gmail.com>
2023-11-11 00:51:27 +0200

    a

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - function that delete node at index point
+ *
+ * @head: The point to first node
+ *
+ * @index: The index of point
+ *
+ * Return: 1 if it succeeded and -1 if not
+*/
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp = *head;
+
+	if (!*head)
+		return (-1);
+
+	for (; index != 0; index--)
+		temp = temp->next;
+	/*check if the index is the first node*/
+	if (temp == *head)
+	{
+		*head = (*head)->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
+	else
+	{
+		temp->prev->next = temp->next;
+		if (temp->next != NULL)
+			temp->next->prev = temp->prev;
+	}
+	free(temp);
+	return (1);
+}
