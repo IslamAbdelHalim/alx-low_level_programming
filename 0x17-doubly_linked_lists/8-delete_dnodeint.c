5a065394b38b998be2437810f96a9a0eef9ed6cc
IslamAhmed <eslamelbeltagye1111@gmail.com>
2023-11-11 00:53:00 +0200

    a

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -18,7 +18,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (-1);
 
 	for (; index != 0; index--)
-		temp = temp->next;
+		if (temp == NULL)
+			temp = temp->next;
 	/*check if the index is the first node*/
 	if (temp == *head)
 	{
