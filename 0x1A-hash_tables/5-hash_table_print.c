a1bec606dc2ca14146068447063e4171597bf008
IslamAbdelHalim <eslamelbeltagye1111@gmail.com>
2023-12-02 02:45:00 +0200

    a

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -23,7 +23,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (flag == 1)
 				printf(", ");
-			printf("%s : %s", item->key, item->value);
+			printf("'%s' : '%s'", item->key, item->value);
 			flag = 1;
 			item = item->next;
 		}
