1df43ffec76065975c755723a88b39404df8e620
IslamAbdelHalim <eslamelbeltagye1111@gmail.com>
2023-12-02 02:42:57 +0200

    a

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function that print hash table
+ *
+ * @ht: The hash table
+*/
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *item;
+	char flag  = 0;
+
+	if (ht == NULL || ht->array == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		item = ht->array[i];
+		while (item != NULL)
+		{
+			if (flag == 1)
+				printf(", ");
+			printf("%s : %s", item->key, item->value);
+			flag = 1;
+			item = item->next;
+		}
+	}
+	printf("}\n");
+}
