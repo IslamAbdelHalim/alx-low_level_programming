88ff8a5d0495d454032a989ef3d3e80ae5580947
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-03-13 19:20:18 +0200

    advanced binary search

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,44 @@
+#include "search_algos.h"
+
+/**
+ * advanced_binary - function that make binary search
+ *
+ * @array: the array to search in
+ *
+ * @size: the size of array
+ *
+ * @value: the value of array
+ *
+ * Return: the index of array
+ */
+
+int advanced_binary(int *array, size_t size, int value)
+{
+  size_t low = 0, high = size - 1, mid, i;
+
+  if (array == NULL || size == 0)
+    return (-1);
+
+  while (low <= high)
+  {
+	  printf("Searching in array: ");
+	  for (i = low; i < high; i++)
+	  {
+	  	printf("%d, ", array[i]);
+	  }
+	  printf("%d\n", array[high]);
+	  mid = (low + high) / 2;
+	  
+	  if (array[mid] == value)
+	  {
+		  if (mid == 0 || array[mid - 1] < value)
+			  return (mid);
+		  high = mid;
+	  }
+	  else if (array[mid] < value)
+		  low = mid + 1;
+	  else
+		  high = mid;
+  }
+  return (-1);
+}
